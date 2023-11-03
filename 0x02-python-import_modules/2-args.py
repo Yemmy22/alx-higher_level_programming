9499232c891ad9606c8cce30320179cecfce3802
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-11-03 15:50:58 +0000

    Print commandline arguments with index

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1:
+        print("{:d} argument.".format(len(sys.argv) - 1))
+    elif len(sys.argv) > 1 and len(sys.argv) < 3:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+        print("{:d}: {}".format(len(sys.argv) - 1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        for args in range(len(sys.argv)):
+            if len(sys.argv) > 1 and args >= 1:
+                print("{:d}: {}".format(args, sys.argv[args]))
