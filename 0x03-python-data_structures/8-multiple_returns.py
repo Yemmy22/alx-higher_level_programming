e2ab863e716dba3d925997ac94d554d6b4e58523
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-11-07 15:03:45 +0000

    Fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in range(len(sentence)):
-        i += 1
-
-    if i == 0:
-        return None
+    if len(sentence) == 0:
+        return 0, None
     else:
-        return i, sentence[0]
+        return len(sentence), sentence[0]
