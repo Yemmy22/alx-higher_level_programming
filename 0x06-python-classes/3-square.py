3b9705e7dac2cf8127c49119a3cc6be36b590945
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-11-25 08:53:15 +0000

    Add a class method that returns the area of square instance

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+An empty Square Class Module.
+'''
+
+
+class Square:
+    '''
+    A Class representing a Square.
+    '''
+    def __init__(self, size=0):
+        '''
+        The init class method initializes any instance of Square
+        with private attribute 'size' which must be a
+        positive integer.
+
+        Parameters:
+            size: The size input of the square.
+        '''
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """ area returns the area of a square."""
+        return self.__size * self.__size
