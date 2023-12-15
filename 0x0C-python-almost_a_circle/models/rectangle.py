591e500ad6c05dab4508d0ac268d270c8149b35b
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-12-15 14:19:40 +0000

    Fix more errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,11 +17,11 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self):
+    def width(self, value):
         '''
         Sets the value of private attribute width.
         '''
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -31,12 +31,11 @@ class Rectangle(Base):
         return self.__height
 
     @height.setter
-    def height(self):
+    def height(self, value):
         '''
         Sets the value of private attribute height.
         '''
-        self.__y = y
-        self.__height = height
+        self.__height = value
 
     @property
     def x(self):
@@ -46,12 +45,11 @@ class Rectangle(Base):
         return self.__x
 
     @x.setter
-    def x(self):
+    def x(self, value):
         '''
         Sets the value of private attribute x.
         '''
-        self.__y = y
-        self.__x = x
+        self.__x = value
 
     @property
     def y(self):
@@ -61,17 +59,17 @@ class Rectangle(Base):
         return self.__y
 
     @y.setter
-    def y(self):
+    def y(self, value):
         '''
         Sets the value of private attribute y.
         '''
-        self.__y = y
+        self.__y = value
 
     def __init__(self, width, height, x=0, y=0, id=None):
         '''
         Initilaizes a rectangle.
         '''
-        Base.__init__(self, id)
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
