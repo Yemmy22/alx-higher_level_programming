c7ea6fc1486596197faf94b9246baf9c9bfd2553
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-01-21 15:11:36 +0000

    Implement the a base class area method and string representation of a rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''
+BaseGeometry Class Module.
+'''
+
+
+class BaseGeometry:
+    '''Define BaseGeometry Class.'''
+    def area(self, width, height):
+        ''' Raises exception when invoked.'''
+        '''raise Exception('area() is not implemented')'''
+        return self.width * self.height
+
+    def integer_validator(self, name, value):
+        '''
+        Checks value is an integer greater than zero or raises
+        exception if otherwise.
+        '''
+        self.name = name
+        if type(value) != int:
+            raise TypeError(self.name + ' must be an integer')
+        elif value <= 0:
+            raise ValueError(self.name + ' must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    '''
+    A BaseGeometry subclass with width and height attributes.
+    '''
+    def __init__(self, width, height):
+        ''' Initializes Rectangle objects.'''
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        '''
+        Returns the area of a rectangle
+        '''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''
+        Returns a user friendly representation of a rectangle
+        '''
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
