4cf18862c814ef162dff472467906de3bf6c0521
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-01-25 18:33:12 +0000

    Create class method with functionality tomodify the attributes of a square object

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,3 +38,28 @@ class Square(Rectangle):
         '''
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        '''
+        Modifies the value of a square's attributes.
+        '''
+        if args and len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                if i == 1:
+                    self.size = args[i]
+                if i == 2:
+                    self.x = args[i]
+                if i == 3:
+                    self.y = args[i]
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'size':
+                    self.size = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
