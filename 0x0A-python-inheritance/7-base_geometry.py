578e0e7a7a53c7568941b5bd6521478105a5c2d7
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-02-27 14:53:05 +0000

    Fix

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,9 +5,14 @@ BaseGeometry Class Module.
 
 
 class BaseGeometry:
-    '''Define BaseGeometry Class.'''
+    '''
+    Define BaseGeometry Class.
+    '''
+
     def area(self):
-        ''' Raises exception when invoked.'''
+        '''
+        Raises exception when invoked.
+        '''
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
