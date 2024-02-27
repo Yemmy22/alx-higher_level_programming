2656bd602fd883783bc4cd7408a3fac345f895ed
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-02-27 15:54:26 +0000

    Fix type check statement in square class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,18 +5,22 @@ BaseGeometry Class Module.
 
 
 class BaseGeometry:
-    '''Define BaseGeometry Class.'''
+    '''
+    Define BaseGeometry Class.
+    '''
+
     def area(self, width, height):
-        ''' Raises exception when invoked.'''
-        '''raise Exception('area() is not implemented')'''
-        return self.width * self.height
+        '''
+        Raises exception when invoked.
+        '''
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
         '''
         Checks value is an integer greater than zero or raises
         exception if otherwise.
         '''
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError(name + ' must be an integer')
         elif value <= 0:
             raise ValueError(name + ' must be greater than 0')
