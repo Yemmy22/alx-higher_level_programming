e2df0d7c8e25cf1502a3c9a922d96d21f06dbab0
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-04-10 19:28:06 +0000

    Hot fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,11 +3,22 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  let argArray = process.argv.slice(2);
-  argArray = argArray.sort().map(Number);
+  const argArray = process.argv.slice(2);
+  for (let i = 0; argArray[i] !== undefined; i++) {
+    let temp;
+    for (let j = i + 1; argArray[j] !== undefined; j++) {
+      if (Number(argArray[j]) < Number(argArray[i])) {
+        temp = Number(argArray[j]);
+        argArray[j] = Number(argArray[i]);
+        argArray[i] = temp;
+      } else {
+        argArray[i] = Number(argArray[i]);
+        argArray[j] = Number(argArray[j]);
+      }
+    }
+  }
 
   const Largest = argArray[argArray.length - 1];
-
   for (let i = 0; argArray[i + 1] !== undefined; i++) {
     if (argArray[i + 1] === Largest) {
       console.log(argArray[i]); break;
