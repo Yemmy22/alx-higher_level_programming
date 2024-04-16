6c742f99a7186052cfbd60422e62f60f67c76ae4
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-04-16 14:06:04 +0000

    Create a new square class that inherits from a square class and prints a matrix with a character

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const baseSquare = require('./5-square');
+
+module.exports = class Square extends baseSquare {
+	charPrint(c) {
+		for (let i = 0; i < this.height; i++) {
+			if (c) {
+				console.log('C'.repeat(this.width));
+			} else {
+				console.log('X'.repeat(this.width));
+			}
+		}
+	}
+};
