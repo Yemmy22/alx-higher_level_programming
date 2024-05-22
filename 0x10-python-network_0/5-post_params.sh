96c2bd74a61e4b58fcbb02bb30bb34604ce2bdc1
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-05-22 12:44:47 +0000

    Include silent option

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends key/value pair data in a post request with curl.
-curl -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
+curl -d "email=test@gmail.com" -d "subject=I will always be here for PLD" -s "$1"
