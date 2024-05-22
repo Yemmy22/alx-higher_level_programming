48b545b8e669bb359912cc071b3a104e38d7ac53
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-05-22 12:25:14 +0000

    Include silent option

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script includes a new header in the HTTP request and displays the body of the response.
-curl -H "X-School-User-Id: 98" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
