c8323d36cbf2a90f2b16ff35a6dd7b5e273eb392
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-05-23 08:41:28 +0000

    Add silent option to the scriptwq

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends in an HTTP POST request, a data from a json file.
-curl -H "Content-Type: application/json" -d @"$2" -X POST "$1"
+curl -s -H "Content-Type: application/json" -d @"$2" -X POST "$1"
