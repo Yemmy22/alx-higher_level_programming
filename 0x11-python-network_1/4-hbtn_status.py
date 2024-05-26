aa978b884f727af849fbf0ac6cc067e4fd496d79
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-05-26 19:59:16 +0000

    Fix omission in code output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,5 +12,6 @@ if __name__ == "__main__":
 
     response = requests.get(url)
     page = response.text
+    print("Body response:")
     print("\t- type:", type(page))
     print("\t- content:", page)
