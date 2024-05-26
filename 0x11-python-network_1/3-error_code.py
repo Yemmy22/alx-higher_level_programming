7ad7728b10b9a384ffc7b3440e0c6d2af0079fcc
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-05-26 19:16:43 +0000

    Fix error in code base

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,14 +8,17 @@ from sys import argv
 from urllib.request import urlopen, Request
 from urllib.error import URLError
 
-url = argv[1]
-req = Request(url)
+if __name__ == "__main__":
 
-try:
-    response = urlopen(req)
-except URLError as e:
-    if hasattr(e, 'code'):
-        print("Error code:",  e.code)
-else:
-    fetch = response.read()
-    print(fetch.decode('utf-8'))
+    url = argv[1]
+    req = Request(url)
+
+    try:
+        with urlopen(req) as response:
+            fetch = response.read()
+
+    except URLError as e:
+        if hasattr(e, 'code'):
+            print("Error code:",  e.code)
+    else:
+        print(fetch.decode('utf-8'))
