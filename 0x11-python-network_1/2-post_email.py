9e2519b301c03b825838f8ec7479349845d2fe0e
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-05-26 18:13:07 +0000

    Post an email in a request and displayed via a response

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+A module that posts data in a HTTP request and displays the datain response.
+'''
+
+import sys
+import urllib.request
+import urllib.parse
+
+url = sys.argv[1]
+data = {'email': sys.argv[2]}
+
+data = urllib.parse.urlencode(data)
+data = data.encode('ascii')
+
+with urllib.request.urlopen(url, data)as response:
+    page = response.read()
+    print(page.decode('utf-8'))
