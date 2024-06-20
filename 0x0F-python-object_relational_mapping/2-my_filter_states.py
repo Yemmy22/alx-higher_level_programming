7a40f17f10469752d47b415c9276310eaa07f28f
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-06-20 23:07:11 +0000

    List all columns that match the fourth command line argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''
+A mysqldb module that lists all states in the hbtn_0e_0_usa
+database, that matches the fourth command line argument.
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) == 5:
+        db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states\
+                WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
