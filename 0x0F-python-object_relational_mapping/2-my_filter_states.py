a4fcce70a53624562ef1df1d68c75f165f6ae999
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-06-21 07:56:46 +0000

    Modify command to becoem case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
         cur = db.cursor()
         cur.execute("SELECT * FROM states\
-                WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+                WHERE BINARY name = '{}' ORDER BY id ASC".format(argv[4]))
         rows = cur.fetchall()
         for row in rows:
             print(row)
