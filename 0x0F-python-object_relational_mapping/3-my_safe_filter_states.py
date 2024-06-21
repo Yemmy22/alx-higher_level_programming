676fddc0a9d31f08e66af651368ea37804d21c2b
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-06-21 08:14:44 +0000

    Safely query a list of matched user input through parameterised sql command

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+A mysqldb module that safely queries the lists all states in
+the hbtn_0e_0_usa database, that matches the fourth command
+line argument.
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) == 5:
+        db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states\
+                WHERE BINARY name = %s ORDER BY id ASC", (argv[4],))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
