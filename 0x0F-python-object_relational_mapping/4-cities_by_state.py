7aefe1c4e11fa3457a40fcf91777f6f9cc983d19
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-06-21 21:53:31 +0000

    List associated cities and states in a database

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''
+A mysqldb module that lists all cities with their respective
+states in the hbtn_0e_4_usa database.
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) == 4:
+        db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+        cur = db.cursor()
+        cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities JOIN states ON states.id = state_id")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
