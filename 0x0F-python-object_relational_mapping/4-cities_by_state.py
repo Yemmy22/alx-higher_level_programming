119bd7b57a0a9227a4b3e1b7e76a3e0ee4c8f5dc
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-06-21 21:58:41 +0000

    Modify sql command to implement order of output

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
         db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
         cur = db.cursor()
         cur.execute("SELECT cities.id, cities.name, states.name\
-                FROM cities JOIN states ON states.id = state_id")
+                FROM cities JOIN states ON\
+                states.id = state_id ORDER BY cities.id ASC")
         rows = cur.fetchall()
         for row in rows:
             print(row)
