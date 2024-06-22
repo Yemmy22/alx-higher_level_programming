5fb6de3205bbd05e36730c7e10d5a37206677b41
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-06-22 17:20:51 +0000

    Connect and create a class of table in a database;

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+An sqlalchemy module that creates a declarative base
+subclass to which a state database table will be mapped.
+'''
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+
+class State(Base):
+    '''
+    Subclass of declarative base class
+    with class attributes.
+    '''
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
