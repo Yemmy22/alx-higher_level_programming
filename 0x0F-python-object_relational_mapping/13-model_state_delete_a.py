65a92c7051418ea22f14b9b5e47f98e40178077b
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-06-23 21:37:18 +0000

    Delete state objects that matches a pattern from the database

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+'''
+A sqlalchemy module that connects to mysql database,
+creates a session, deletes all state object with letter 'a'
+in the table.
+'''
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, declarative_base
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_list = session.query(State).filter(State.name.like('%a%')).all()
+    for row in state_list:
+        session.delete(row)
+    session.commit()
