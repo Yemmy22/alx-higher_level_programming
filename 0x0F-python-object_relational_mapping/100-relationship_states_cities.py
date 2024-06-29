1cf516ad0da72032f7db5eb1632b7e0495b88c49
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-06-29 20:14:53 +0000

    Inserts related data of aone to many bidirectional fashion in state and cities table of a new database

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+'''
+A sqlalchemy module that connects to mysql database,
+creates a session, and inserts
+associated data in the state's and citie's tables.
+'''
+
+
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3],
+        pool_pre_ping=True
+        ))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name = 'California')
+    new_city = City(name = 'San Francisco', state = new_state)
+    session.add_all([new_state, new_city])
+    session.commit()
+    session.close()
