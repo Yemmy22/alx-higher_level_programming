5878721e7ea273090386b40aa9a587aaaa39d738
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-07-30 23:54:22 +0000

    Filters the movie api with the character api for the number of appearances of the character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+if (process.argv.length === 3) {
+  request(process.argv[2], (error, response, body) => {
+    if (error) {
+      console.error(error);
+    }
+
+    const results = JSON.parse(body).results;
+    let count = 0;
+    for (const i of results) {
+      for (const j of i.characters) {
+        if (j === characterUrl) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  });
+}
